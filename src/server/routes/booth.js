760298df955c468d88482177e04054392a1a0028
Benjamin Skirlo <internetbewohner@gmail.com>
2017-09-18 12:10:07 +0200

    more nice default links

--- src/server/routes/booth.js
+++ src/server/routes/booth.js
@@ -1,17 +1,13 @@
-#!/usr/bin/env node
-'use strict'
 const config = require('config')
 const turn = require('../turn.js')
 const router = require('express').Router()
 const uuid = require('uuid')
-var Moniker = require('moniker');
-var names = Moniker.generator([Moniker.adjective, Moniker.noun]);
 
 const BASE_URL = config.get('baseUrl')
 const cfgIceServers = config.get('iceServers')
 
 router.get('/', (req, res) => {
-  res.redirect(`${BASE_URL}/call/${names.choose()}`)
+  res.redirect(`${BASE_URL}/call/${uuid.v4()}`)
 })
 
 router.get('/:callId', (req, res) => {
